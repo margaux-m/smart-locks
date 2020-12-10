b7721f1634ec524a906e3c8d5a8638c147fde8e5
Margaux Mansanarez <margaux.mansanarez@gmail.com>
2020-12-10 14:38:32 +0100

    defined handle action in the reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,15 +5,27 @@ class ReportsController < ApplicationController
   before_action :authenticate_server
 
   def handle
-  #   report = params[:report].open
-  #   csv_options = { col_sep: ',', headers: :first_row }
+    # We process the csv file sent in the request.
+    report = params[:report].open
+    csv_options = { col_sep: ',', headers: :first_row }
 
-  #   CSV.parse(report, csv_options) do |timestamp, lock_id, kind, status_change|
-  #     Lock.create(id: lock_id[1], kind: kind[1], status: status_change[1])
-  #     Entry.create(timestamp: timestamp[1], status_change: status_change[1], lock: lock)
-  #   end
-  #   render json: { message: "Successful request! Your report has been processed and includes now #{Lock.count} locks and #{Entry.count} entries." }
-  # end
+    CSV.parse(report, csv_options) do |timestamp, lock_id, kind, status_change|
+      # We check if the lock already exists.
+      lock = Lock.find_by_id(lock_id[1])
+      if lock
+        # If it exists, we update the status.
+        lock.status = status_change[1]
+        lock.save
+      else
+        # It not, we create a new lock.
+        Lock.create(id: lock_id[1], kind: kind[1], status: status_change[1])
+      end
+      # We create a new row/entry with the data extracted.
+      Entry.create(timestamp: timestamp[1], status_change: status_change[1], lock_id: lock_id[1])
+    end
+    # We deliver a message to let the user know it was successfully processed.
+    render json: { message: "Successful request! Your report has been processed and includes now #{Lock.count} locks and #{Entry.count} entries." }
+  end
 
   private
 
